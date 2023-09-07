1cb48dcc5300ef3639e4afe69dacae7b66e831dc
pizzaYami <cyj9407@naver.com>
2023-09-07 11:09:02 +0900

    [feat] mongoDB 사용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h4 className="title">애플후레시</h4>
-      <p className="title-sub">by dev</p>
-    </div>
-  );
+  return <div></div>;
 }
